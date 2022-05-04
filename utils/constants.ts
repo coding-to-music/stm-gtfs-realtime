82da1f82c49b3cd35ffbf6af189d713f525a2ffc
coding-to-music <connors.tom@gmail.com>
2022-05-04 04:52:32 +0000

    Build

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,6 @@
 // Expose environment variables to the browser by prefixing with NEXT_PUBLIC_
 
 export const APIKEY = process.env.STM_API_KEY as string;
-export const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN as string;
+export const MAPBOX_ACCESS_TOKEN = process.env
+  .NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
 export const IS_PROD = process.env.VERCEL_ENV === "production" ? true : false;
